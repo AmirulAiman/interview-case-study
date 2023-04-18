9d8ef3fbb5c2e5f38a7858cd219082c75e19994c
Amirul Aiman <aiman_amirul@ymail.com>
2023-04-18 12:33:08 +0800

    Modify product detail page

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -18,8 +18,32 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Product detail page") }}
+                <div class="grid grid-cols-1 sm:grid-cols-2 sm:gap-3 px-5 py-2">
+                    <div class="px-5 py-4">
+                        <img class="" src="{{ asset('images/'.$product->img_name) }}" alt="{{ $product->name }}">
+                        <span class="bg-blue-200 text-blue-800 rounded-md px-3 py-2">#{{ $product->brand->brand }}</span>
+                        <span class="bg-blue-200 text-blue-800 rounded-md px-3 py-2">#{{ $product->category->category }}</span>
+                    </div>
+                    <div class="w-3/5 mx-auto">
+                        <div class="mb-6">
+                            <h2 class="font-semibold text-2xl">Description</h2>
+                            <hr>
+                            <p>{{ $product->description }}</p>
+                        </div>
+                        <div class="mb-6">
+                            <h2 class="font-semibold text-2xl">Price</h2>
+                            <hr>
+                            <p class="text-lg"></p>RM {{  $product->base_price }}
+                        </div>
+                        <form action="{{ route('cart.store') }}" method="POST">
+                            @csrf
+                            <button type="submit"
+                                class="flex-grow flex justify-center bg-yellow-800 hover:bg-yellow-500/90 hover:text-gray-800 px-6 py-2 rounded-md text-white font-medium tracking-wider transition"
+                                name="product_id" value="{{ $product->id }}">
+                                Add to Cart
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
